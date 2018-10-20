9ab180fd62ec1505dec3421cca63ed9594f4faff
alexzwir <alexzwir@gmail.com>
2018-10-20 07:53:26 -0300

    Adding the description of the project.

---
+++ main.py
@@ -0,0 +1,10 @@
+"""
+A ideia desse script é checar se os usuário que tentaram se cadastrar no site, efetivamente conseguiram ou não. Com isso, teremos três passos:
+- Procurar pelo CPF da chamada da API (definida na tabela com atributo -> )
+- Retirar e guardar em uma lista.
+- Comparar com a base de usuário do site.
+
+The ideia of this project is to search for as specific user ID(CPF) and see if this user it's an register user.
+
+
+"""
